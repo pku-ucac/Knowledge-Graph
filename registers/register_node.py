719494c1b2bef34a88504a986a83c8bda2eb728c
haokou <1766123063@qq.com>
2025-08-01 19:02:12 +0800

    fix bug in nodes.json generating

--- registers/register_node.py
+++ registers/register_node.py
@@ -14,7 +14,7 @@ if len(sys.argv) > 2:
 filenames = [file.name for file in input_path.iterdir() if file.is_file() & file.name.count('#')==1]
 ids_names = list(map(lambda s: s.split('#'), filenames))
 ids, names = tuple(map(list, zip(*ids_names)))
-result = [{"id":"n"+id_prefix+"/"+id, "name":name, "type":"node", "path":input_path.joinpath(filename).as_posix()} for id, name, filename in zip(ids, names, filenames)]
+result = [{"id":"n"+id_prefix+"/"+id, "name":name.split('.')[0], "type":"node", "path":input_path.joinpath(filename).as_posix()} for id, name, filename in zip(ids, names, filenames)]
 
 with open(current_path.joinpath(output_filename), "w", encoding="utf-8") as output:
     json.dump(result, output, indent=4, ensure_ascii=False)
